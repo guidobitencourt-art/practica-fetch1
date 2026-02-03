05ed39fd652a95c717a06a04d727be3c4c28ab9f
Guilherme Bitencourt <guidobitencourt@alumnos.cei.es>
2026-02-03 21:42:46 +0100

    Refactor comments in practica1.js for clarity and consistency

--- practica1.js
+++ practica1.js
@@ -1,9 +1,9 @@
-Crea una función llamada searchPost(id).
-Dentro del try, concatena el id recibido en la URL de la API: https://jsonplaceholder.typicode.com/posts/ID.
-Usa Template Literals para construir la URL.
-Si la petición es exitosa, imprime el título del post en mayúsculas.
-Si ocurre un error (por ejemplo, si el ID no existe o no hay internet), el catch debe imprimir: "El post no pudo ser hallado".
-Subelo a GitHub
+// Crea una función llamada searchPost(id).
+// Dentro del try, concatena el id recibido en la URL de la API: https://jsonplaceholder.typicode.com/posts/ID.
+// Usa Template Literals para construir la URL.
+// Si la petición es exitosa, imprime el título del post en mayúsculas.
+// Si ocurre un error (por ejemplo, si el ID no existe o no hay internet), el catch debe imprimir: "El post no pudo ser hallado".
+// Subelo a GitHub
 
 async function searchPost(id) {
     try {
